2580f777365e93869c2adb2747ecda923916f3fb
aakash02 <aakash.gurung02@gmail.com>
2015-03-31 21:00:23 +0545

    portfolio updated

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    <title>Aakash</title>
+
+     
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/timeline.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/js/fancybox/jquery.fancybox.css" rel="stylesheet" />
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body data-spy="scroll" data-offset="0" data-target="#theMenu">
+		
+	<!-- Menu -->
+	<nav class="menu" id="theMenu">
+		<div class="menu-wrap">
+			<h1 class="logo"><a href="index.html#home">Aakash</a></h1>
+			<i class="fa fa-times menu-close"></i>
+			<a href="#home" class="smoothScroll">Home</a>
+			<a href="#about" class="smoothScroll">About</a>
+			<a href="#timeline" class="smoothScroll">Timeline</a>
+			<a href="#portfolio" class="smoothScroll">Portfolio</a>
+			<a href="#services" class="smoothScroll">Testimony</a>
+			<a href="#contact" class="smoothScroll">Contact</a>
+			<a href="https://www.facebook.com/eyeofdeadakashic"><i class="fa fa-facebook"></i></a>
+			<a href="https://twitter.com/AakashGurung02"><i class="fa fa-twitter"></i></a>
+			<a href="http://np.linkedin.com/pub/aakash-gurung/b3/112/336/en"><i class="fa fa-linkedin"></i></a>
+		    <a href="https://github.com/Aakashgurung02"><i class="fa fa-github"></i></a>
+		   <a href="cv.pdf"> <button class="btn btn-warning">Download my CV</button></a>
+		</div>
+
+		
+		<!-- Menu button -->
+		<div id="menuToggle"><i class="fa fa-bars"></i></div>
+	</nav>
+
+	<section id="home" name="home"></section>
+	<div id="headerwrap">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<h1>Aakash</h1>
+					<h2><FONT color="white">php programmer and designer </FONT></h2>
+					</div>
+				</div>
+			</div><! --/row -->
+		</div><! --/container -->
+	</div><! --/headerwrap -->
+	
+	<section id="about" name="about"></section>
+	<div id="aboutwrap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 name">
+					<img class="img-responsive" src="assets/img/pic.png">
+					<p>AAKASH GURUNG</p>
+					<div class="name-label"></div>
+				</div><! --/col-lg-4-->
+				<div class="col-lg-8 name-desc">
+					<h2>ABOUT ME</h2>
+					<div class="name-zig"></div>
+					<div class="col-md-6">
+						<p>I am a final-year student in Computing at the British college, Kathmandu, Nepal. I expect to receive the Bachelor degree in the summer of 2015. I have learned and experience about advanced web development. I am able to develop a web designing and coding. I spend most of my spare time designing programs and web pages because I enjoy problem solving and logical thinking.</p>
+						<p></p>
+					</div>
+					<div class="col-md-6">
+						<p>Most of the time i worked on php, css, html, photoshop, mysql etc while designing and developing the websites.</p>
+						<p></p>
+					</div>
+					</div><! --/col-lg-8-->
+	</div>
+<div class="col-lg-8 col-lg-offset-2 mt">
+						
+							<h2>Skills</h2>
+							<div class="progress">
+  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar"
+  aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+    50% PHP
+  </div>
+</div>
+
+<div class="progress">
+  <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar"
+  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:35%">
+   35% Java
+  </div>
+</div>
+
+<div class="progress">
+  <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar"
+  aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%">
+    60% Photoshop
+  </div>
+</div>
+
+<div class="progress">
+  <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar"
+  aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+    70% HTML/CSS
+  </div>
+</div>
+
+<div class="progress">
+  <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar"
+  aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:45%">
+    45% MySQL
+  </div>
+</div>
+</div>
+
+							
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /aboutwrap -->
+	<! -- PORTFOLIO SEPARATOR -->
+	
+
+	<! -- TIMELINE SECTION -->
+	
+	<section id="timeline" name="portfolio"></section>
+	<div id="portfoliowrap">
+   <h1>Education</h1>
+	<div class="container">
+    <ul class="timeline">
+        <li class="timeline-inverted">
+          <div class="timeline-badge " ><i class="glyphicon glyphicon" style="font-size:15px;" >2015</i></div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title">Bsc Computing<br/><small>Leeds Beckett University</small></h4>
+            </div>
+            <div class="timeline-body">
+              <p>Running Bsc Computing at The British College, Kathmandu, Nepal</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="timeline-badge danger"><i class="glyphicon glyphicon" style="font-size:15px;" >2014</i></div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title">HND<br/><small>Edexcel</small></h4>
+            </div>
+            <div class="timeline-body">
+              <p>Completed HND from Internation School of Management and Technology, Kathmandu, Nepal</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge warning"><i class="glyphicon glyphicon" style="font-size:15px;" >2011</i></div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title">Foundation Year<br/><small>NCC</small></h4>
+            </div>
+            <div class="timeline-body">
+              <p>Completed Foundation year from Softwarica College of IT & E-commerce, Kathmandu, Nepal</p>
+            </div>
+          </div>
+        </li>
+        <li>   
+        <li>
+          <div class="timeline-badge info"><i class="glyphicon glyphicon" style="font-size:15px;" >2010</i></div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title">Higher Secondary<br/><small>HSEB</small></h4>
+            </div>
+            <div class="timeline-body">
+              <p>Completed Higher Secondary from Shanti Academy, Bharatpur, Nepal</p>
+              <hr>
+              <div class="btn-group">
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge success"><i class="glyphicon glyphicon" style="font-size:15px;" >2008</i></div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title">SCHOOL LEAVING CERTIFICATE<br/><small>Goverment of Nepal</small></h4>
+            </div>
+            <div class="timeline-body">
+              <p>Completed SLC from Srijana English Boarding School, Bharatpur, Nepal</p>
+            </div>
+          </div>
+        </li>
+    </ul>
+</div>
+</div>
+		<! -- ABOUT SEPARATOR -->
+	
+
+	<! -- PORTFOLIO SECTION -->
+	<section id="portfolio" name="portfolio"></section>
+	<div id="portfoliowrap">
+		<div class="container">
+			<div class="row">
+				<h1>SOME OF MY LATEST WORKS</h1>
+			<div class="row mt">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+					<div class="project-wrapper">
+	                    <div class="project">
+	                        <div class="photo-wrapper">
+	                            <div class="photo">
+	                            
+	                            	<a class="fancybox" href="assets/img/portfolio/port01.jpg">
+
+
+	                            	<img class="img-responsive" src="assets/img/portfolio/port01.jpg" alt=""></a>
+	                            </div>
+	                            <div class="overlay"></div>
+	                            &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;<button><a href="http://blog.aakash02.com.np">VISIT PAGE</a></button>
+	                        </div>
+	                    </div>
+	                </div>
+				</div><!-- col-lg-4 -->
+				
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+					<div class="project-wrapper">
+	                    <div class="project">
+	                        <div class="photo-wrapper">
+	                            <div class="photo">
+	                            	<a class="fancybox" href="assets/img/portfolio/port02.jpg"><img class="img-responsive" src="assets/img/portfolio/port02.jpg" alt=""></a>
+	                            </div>
+	                            <div class="overlay"></div>
+	                            &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class="default"><a href="http://blog1.aakash02.com.np">VISIT PAGE</a></button>
+	                        </div>
+	                    </div>
+	                </div>
+				</div><!-- col-lg-4 -->
+				
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+					<div class="project-wrapper">
+	                    <div class="project">
+	                        <div class="photo-wrapper">
+	                            <div class="photo">
+	                            	<a class="fancybox" href="assets/img/portfolio/port03.jpg"><img class="img-responsive" src="assets/img/portfolio/port03.jpg" alt=""></a>
+	                            </div>
+	                            <div class="overlay"></div>
+	                            <div class="overlay"></div>
+	                            &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button><a href="http://aakash02.com.np">VISIT PAGE</a></button>
+	                        </div>
+	                    </div>
+	                </div>
+				</div><!-- col-lg-4 -->
+			</div><!-- /row -->
+		</div><
+	</div><! --/Portfoliowrap -->
+
+	<! -- PORTFOLIO SEPARATOR -->
+		
+	
+	
+
+	
+	
+	<! -- SERVICE SECTION -->
+	<section id="services" name="services"></section>
+	<div id="servicewrap">
+		<div class="container">
+		
+				<h1>Testimonial</h1>
+		</div><! --/container -->
+	</div><! --/servicewrap -->
+	
+	<div id="testimonials">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 mt">
+				
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					  <!-- Wrapper for slides -->
+					  <div class="carousel-inner">
+					    <div class="item active mb centered">
+					      <h3>Arjun KC (web developer)</h3>
+					      <p>Has good knowledge in designing and giving new ideas.</p>
+					      <p><img class="img-circle" src="assets/img/pic-t1.jpg" width="80"></p>
+					    </div>
+
+					    <div class="item mb centered">
+					      <h3>Sagar Karki (system analyst)</h3>
+					      <p>Helps in designing parts and gives solution to the webpage related problems.</p>
+					      <p><img class="img-circle" src="assets/img/pic-t2.jpg" width="80"></p>
+					    </div>
+
+					    <div class="item mb centered">
+					      <h3>Prakash Thapa (android developer)</h3>
+					      <p>Has new concept of new system for the future and hard working.</p>
+					      <p><img class="img-circle" src="assets/img/pic-t3.jpg" width="80"></p>
+					    </div>
+
+					  </div>
+					  <!-- Indicators -->
+					  <ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					  </ol>
+					</div>
+					  
+				</div><! --/col-lg-8 -->
+			</div><! --/row -->
+		</div><! --/container -->
+	</div><! --/testimonials -->
+	
+	<! -- SERVICE SECTION -->
+	<section id="contact" name="contact"></section>
+	<! -- CONTACT SEPARATOR -->
+	<div id="contactwrap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<p>CONTACT ME RIGHT NOW!</p>
+					<p>
+						<small>Lazimpat, 07<br/>
+						Kathmandu,<br/>
+						Nepal.</small>
+					</p>
+					<p>
+						<small>Tel. 9888-4394<br/>
+						Mail. aakash.gurung02@gmail.com<br/>
+					</p>
+			
+				</div>
+				
+				<div class="col-lg-6">
+					<form role="form">
+					  <div class="form-group">
+					    <label for="exampleInputName1">Your Name</label>
+					    <input type="text" class="form-control" id="InputText" placeholder="Enter name">
+					    <label for="exampleInputEmail1">Email address</label>
+					    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+					    <label for="exampleInputText1">Message</label>
+					    <textarea class="form-control" rows="3"></textarea>
+					  </div>
+					  <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+				</div>
+			
+			</div><! --/row -->
+		</div><!-- /container -->
+	</div>
+	
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="assets/js/classie.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/smoothscroll.js"></script>
+	<script src="assets/js/jquery.stellar.min.js"></script>
+	<script src="assets/js/fancybox/jquery.fancybox.js"></script>    
+	<script src="assets/js/main.js"></script>
+		<script>
+		$(function(){
+			$.stellar({
+				horizontalScrolling: false,
+				verticalOffset: 40
+			});
+		});
+		</script>
+		
+		<script type="text/javascript">
+      $(function() {
+        //    fancybox
+          jQuery(".fancybox").fancybox();
+      });
+	   </script>
+
+  </body>
+</html>
